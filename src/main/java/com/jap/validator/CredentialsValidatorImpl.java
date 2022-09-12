04a1a5c9a3a07c2837ad110fc06381348337f7a8
Revathi G <revathikrishna0323@gmail.com>
2022-09-12 13:07:56 +0530

    change done in CredentialsValidatorImpl.java

--- src/main/java/com/jap/validator/CredentialsValidatorImpl.java
+++ src/main/java/com/jap/validator/CredentialsValidatorImpl.java
@@ -4,7 +4,7 @@ public class CredentialsValidatorImpl {
     public String validateUser(String name, String password) {
         CredentialsValidator credentialsValidator = ((userId, password_1) -> {
             if (name.equals("admin") && password.equals("1234")) {
-                return " valid user ";
+                return "valid user";
             } else {
                 return "invalid user";
             }
